5416ef9ae2944ca1d2f3ddd9f3c714149854a6eb
Joshua LaBelle <labelljd@gmail.com>
2025-03-27 21:00:26 -0400

    after AoN #8 configuring LSP

--- init.lua
+++ init.lua
@@ -5,18 +5,22 @@ require("config.lazy")
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>") -- source current file
-vim.keymap.set("n", "<space>x", ":.lua<CR>") -- run current line
-vim.keymap.set("v", "<space>x", ":lua<CR>") -- run visually selected
+vim.keymap.set("n", "<space>x", ":.lua<CR>")                -- run current line
+vim.keymap.set("v", "<space>x", ":lua<CR>")                 -- run visually selected
+
+-- Needed this line to show the text on lsp warnings/errors - appears not needed anymore once lspconfig is set up...
+vim.diagnostic.config({ virtual_text = true })
 
 -- Highlight when yanking (copying) text
 -- Try it with 'yap' in normal mode
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
