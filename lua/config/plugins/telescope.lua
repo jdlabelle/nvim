9fdc1d4518897d4770ef883a5dd2b0d1beb0eb3f
Joshua LaBelle <labelljd@gmail.com>
2025-03-28 16:46:47 -0400

    after AoN #9, Telescope added

---
+++ lua/config/plugins/telescope.lua
@@ -0,0 +1,27 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+  },
+  config = function()
+    require('telescope').setup {
+      pickers = {
+        find_files = {
+          theme = "ivy"
+        }
+      },
+      extensions = {
+        fzf = {}
+      }
+    }
+    vim.keymap.set("n", "<leader>fh", require('telescope.builtin').help_tags, { desc = 'Telescope search help' })
+    vim.keymap.set("n", "<leader>fd", require('telescope.builtin').find_files, { desc = 'Telescope find files' })
+    vim.keymap.set("n", "<leader>en", function()
+      require('telescope.builtin').find_files {
+        cwd = vim.fn.stdpath("config")
+      }
+    end, { desc = 'find nvim config files' })
+  end
+}
