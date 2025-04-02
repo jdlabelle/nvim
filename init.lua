dcfe83c550c98bc859db3204c9f67dba69d01871
Joshua LaBelle <labelljd@gmail.com>
2025-04-02 12:13:57 -0400

    after AoN #16, custom floating term + vimtex

--- init.lua
+++ init.lua
@@ -7,10 +7,11 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")                           -- Jump forward to the next item in quickfix
-vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")                           -- Jump backward to the previous item in quickfix
-vim.keymap.set("n", "<leader>qf", ":lua vim.diagnostic.setqflist()<CR>") -- Set the quickfix list with all diagnostic issues from buffers
-vim.keymap.set("n", ",v", "<C-v>")                                       -- needed for WSL
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>", { desc = "Jump forward to the next item in quickfix" })
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>", { desc = "Jump backward to the previous item in quickfix" })
+vim.keymap.set("n", "<leader>qf", ":lua vim.diagnostic.setqflist()<CR>",
+    { desc = "Set the quickfix list with all diagnostic issues from buffers" })
+vim.keymap.set("n", ",v", "<C-v>", { desc = "Visual block mode for WSL" })
 -- inactive (using Oil.nvim) vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>") -- source current file
@@ -24,39 +25,39 @@ vim.diagnostic.config({ virtual_text = true })
 -- Try it with 'yap' in normal mode
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 vim.api.nvim_create_autocmd('TermOpen', {
-  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
+    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
 })
 
 -- Open the built-in nvim terminal in the specified format
 local job_id = 0
 vim.keymap.set("n", "<leader>st", function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 15)
-  job_id = vim.bo.channel -- goes with below
-end)
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 15)
+    job_id = vim.bo.channel -- goes with below
+end, { desc = "Open built-in nvim terminal" })
 
 -- Run a custom command in the nvim terminal (you can be in a different window)
 vim.keymap.set("n", "<leader>tr", function()
-  -- make, python test, etc
-  -- some bash command to run
-  vim.fn.chansend(job_id, { "tree .\r\n" })
-end)
+    -- make, python test, etc
+    -- some bash command to run
+    vim.fn.chansend(job_id, { "tree .\r\n" })
+end, { desc = "Run custom terminal command" })
 
-vim.keymap.set("t", "<leader>tx", "<C-\\><C-N>") -- exit terminal mode
+vim.keymap.set("t", "<leader>tx", "<C-\\><C-N>", { desc = "Exit built-in terminal" })
 
 -- Oil.nvim
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -3,21 +3,23 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require'nvim-treesitter.configs'.setup {
+      require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
         ensure_installed = { "python", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        sync_install = false,
 
         -- Automatically install missing parsers when entering buffer
         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
         auto_install = false,
+        ignore_install = { "latex" },
         highlight = {
           enable = true,
           disable = function(lang, buf)
-              local max_filesize = 100 * 1024 -- 100 KB
-              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-              if ok and stats and stats.size > max_filesize then
-                  return true
-              end
+            local max_filesize = 100 * 1024   -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
           end,
           additional_vim_regex_highlighting = false,
         },
