7a7b67d8447b402260bbc8b01b8949b136f0c953
Joshua LaBelle <labelljd@gmail.com>
2025-04-07 22:15:11 -0400

    update .tex specific file settings

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -3,5 +3,13 @@ local set = vim.opt_local
 set.shiftwidth = 2
 set.tabstop = 2
 set.expandtab = true
-set.textwidth = 80
+set.colorcolumn = "80"
+--set.wrap = true
+--set.linebreak = true
+vim.keymap.set('n', '<leader>wc', '<cmd>VimtexCountWords!<CR>',
+  { buffer = true, desc = 'Vimtex Word Count' })
+set.textwidth = 79
 set.formatoptions:append("t")
+-- Enable spell checking only for spellable regions (like text, not code/math)
+set.spell = true
+set.spelllang = { 'en_us' }
