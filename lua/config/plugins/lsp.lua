98b93d59371bef59a6024081d8ca3477e90bf81b
Joshua LaBelle <labelljd@gmail.com>
2025-04-08 21:36:24 -0400

    add whichkey plugin and configure python lsp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -18,6 +18,7 @@ return {
     config = function()
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       require("lspconfig").lua_ls.setup { capabilities = capabilities }
+      require("lspconfig").pyright.setup { capabilities = capabilities }
 
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('my.lsp', {}),
