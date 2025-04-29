d946ae4e32ec236ff1e91a9c7242837cf74d4887
Joshua LaBelle <labelljd@gmail.com>
2025-04-28 21:32:52 -0400

    add config options for vimtex and update lockfile

--- lua/config/plugins/vimtex.lua
+++ lua/config/plugins/vimtex.lua
@@ -6,6 +6,15 @@ return {
     config = function()
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_view_method = "zathura_simple"
+      vim.g.vimtex_quickfix_open_on_warning = 0
+
+      local au_group = vim.api.nvim_create_augroup("vimtex_events", {})
+      -- Cleanup on quit (change to VimtexClean! to also remove output files)
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VimtexEventQuit",
+        group = au_group,
+        command = "VimtexClean"
+      })
     end
   }
 }
