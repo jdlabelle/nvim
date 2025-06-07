738f67275bb214fc53165ead7186ec34ca87ce7e
Joshua LaBelle <labelljd@gmail.com>
2025-06-07 16:36:21 -0400

    add mini.ai and mini.surround

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,11 +1,25 @@
 return {
-    {
-        'echasnovski/mini.nvim',
-        -- change below to `false` to disable the plugin!
-        enabled = true,
-        config = function()
-            local statusline = require 'mini.statusline'
-            statusline.setup { use_icons = true }
-        end
-    }
+  {
+    'echasnovski/mini.nvim',
+    -- change below to `false` to disable the plugin!
+    enabled = true,
+    config = function()
+      -- Better Around/Inside textobjects
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- Simple and easy statusline
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = true }
+    end
+  }
 }
