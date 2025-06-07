88e2f00f610e60ea2e56d0c22fed4c3e3463fc29
Joshua LaBelle <labelljd@gmail.com>
2025-06-07 12:27:02 -0400

    add render-markdown, remove markview

--- lua/config/plugins/markview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "OXY2DEV/markview.nvim",
-  lazy = false,
-  enabled = false,
-  -- For blink.cmp's completion source
-  dependencies = {
-    "saghen/blink.cmp"
-  },
-};
---
+++ lua/config/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      enabled = true,
+      completions = { blink = { enabled = true } },
+    },
+  }
+}
